6732e7b877102c1e89ef18dc9cb4bbc10e628d46
utilizer-code-commit-user-at-484599503487 <goksgreat@gmail.com>
2018-12-13 17:49:08 +0530

    Fixed Production Build

---
+++ webpack.config.prod.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, "public/index.html"),
+    filename: "./index.html"
+});
+
+const WebpackProdServerUrl = 'http://goksgreat.com/assignment';
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src') + '/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+          test: /\.(ttf|eot|otf|svg|png)$/,
+          loader: 'file-loader'
+      },
+      {
+          test: /\.(woff|woff2)$/,
+          loader: 'url-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: `${WebpackProdServerUrl}/`,
+    filename: 'bundle.js'
+    },
+    plugins: [
+      htmlWebpackPlugin
+    ]
+};
